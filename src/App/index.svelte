39994f7f5ba2f5553493fa553289f4bd67f533dd
Martin Maugeais <mrtnmgs@gmail.com>
2021-01-21 13:20:13 -0500

    svetle boilerplate

---
+++ src/App/index.svelte
@@ -0,0 +1,19 @@
+<script>
+    import Layout from '@Components/Layout';
+    import Circle from '@Components/Circle';
+</script>
+
+<style src="./style.scss">
+
+</style>
+
+<Layout>
+    <div class="container">
+        <div class="container__top">
+            <Circle />
+        </div>
+        <div class="container__middle">
+            Hello World
+        </div>
+    </div>
+</Layout>
---
+++ src/Components/Circle/index.svelte
@@ -0,0 +1,44 @@
+<script>
+const graphSize = 500;
+const svgSize = graphSize + 0.1 * graphSize; // making sure nothing gets clipped
+
+let led = {
+  size: graphSize / 100,
+  fill: "orange",
+  x: graphSize / 2,
+  y: graphSize / 2
+};
+
+const euclid = {
+  steps: 7,
+  notes: 13
+};
+
+var round = (number) => {
+  return parseFloat((Math.floor(number * 100) / 100).toFixed(2));
+}
+
+var poltocar = ( half, angle ) => {
+  var radius = 10;
+  var x = round(
+      half + half * radius * Math.cos((Math.PI * angle) / 180)
+      );
+  var y = round(
+      half + half * radius * Math.sin((Math.PI * angle) / 180)
+      );
+  return { x, y };
+}
+
+const intervalId = setInterval(() => {
+  led.x = led.x + 5;
+  led.x > graphSize && led.x = 0;
+}, 500);
+
+
+</script>
+<style src="./style.scss">
+
+</style>
+<svg width={svgSize} height={svgSize}>
+<circle fill={led.fill} cx={led.x} cy={led.y} r={led.size} />
+</svg>
