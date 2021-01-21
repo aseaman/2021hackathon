bd432d77a7eceed47e9b02957b20007aa13e63fe
Aaron Seaman <aaron.seaman@gmail.com>
2021-01-21 11:47:18 -0600

    fleshing out euclidean rhythm algorithm

---
+++ src/euclidean_rhythm.js
@@ -0,0 +1,39 @@
+function generateEuclideanRhythm(pulses, steps) {
+	const slope = pulses / steps;
+	const euclidean = [];
+	let previous = -1;
+
+	for (let i = 0; i < steps; i++) {
+		const current = Math.floor(i * slope);
+		euclidean.push(current !== previous ? 1 : 0);
+		previous = current;
+	}
+
+	return euclidean;
+}
+
+function recursiveGenerator(pulses, steps) {
+	const hits = Array(pulses).fill(1);
+	const rests = Array(steps - pulses).fill(0);
+
+	return recurse(hits, rests);
+}
+
+function recurse(hits, rests) {
+	if (rests.length <= 1) {
+		return hits
+			.concat(rests)
+			.flatMap((e) => e.split(""))
+			.map((e) => parseInt(e, 10));
+	}
+
+	const newHits = [];
+	while (hits.length && rests.length) {
+		newHits.push(`${hits.pop()}${rests.pop()}`);
+	}
+
+	return recurse(newHits, hits.concat(rests));
+}
+
+exports.generateEuclideanRhythm = generateEuclideanRhythm;
+exports.recursiveGenerator = recursiveGenerator;
