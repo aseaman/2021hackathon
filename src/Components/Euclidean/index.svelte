1015b4c85166df87f843ce43a02984e4d096187c
Aaron Seaman <aaron.seaman@gmail.com>
2021-01-22 12:25:52 -0600

    added different voices

--- src/Components/Euclidean/index.svelte
+++ src/Components/Euclidean/index.svelte
@@ -1,6 +1,5 @@
 <script>
   export let tick = 0;
-//  import * as Tone from "tone";
   export let Tone; // importing dynamically because of an issue in Chrome, see https://github.com/Tonejs/Tone.js/issues/443
   import generateEuclideanRhythm from '../../euclidean_rhythm';
   import Circle from '@Components/Circle';
@@ -17,8 +16,21 @@
   let isPlaying = false;
   let onsets = 3;
   let steps = 7;
+  let voice = 'synth';
 
   const synth = new Tone.MembraneSynth().toDestination();
+  const drumTest = new Tone.Sampler({
+    A1: 'snare.mp3',
+    A2: 'kick.mp3',
+    A3: 'hihat.mp3',
+    A4: 'tom1.mp3'
+    },
+    {
+      volume: 5,
+      release: 1,
+      baseUrl: 'https://tonejs.github.io/audio/drum-samples/Kit8/'
+    }
+  ).toDestination();
 
   function handleGeneratePattern() {
     pattern = generateEuclideanRhythm(onsets, steps);
@@ -34,13 +46,41 @@
     } else {
       circlepat = pattern;
       active = 0;
-      const notes = pattern.map((e, index) => e ? 'B1' : null);
+      let notes = [];
+      if (voice === 'synth') {
+        notes = pattern.map((e, index) => e ? 'B2' : null);
+      } else {
+        let drumNote = '';
+        switch (voice) {
+          case 'snare':
+            drumNote = 'A1';
+            break;
+          case 'kick':
+            drumNote = 'A2';
+            break;
+          case 'hihat':
+            drumNote = 'A3';
+            break;
+          case 'tom':
+            drumNote = 'A4';
+            break;
+          default:
+            drumNote = 'A1';
+            break;
+        }
+        notes = pattern.map((e, index) => e ? drumNote : null);
+      }
+      console.log(notes);
       if (sequence && !sequence.disposed) {
         sequence.dispose();
       }
       sequence = new Tone.Sequence((time, note) => {
         active = (active + 1) % circlepat.length
-        synth.triggerAttackRelease(note, "8n", time);
+        if (voice === 'synth') {
+          synth.triggerAttackRelease(note, "8n", time);
+        } else {
+          drumTest.triggerAttackRelease(note, "8n", time);
+        }
       }, notes).start(0);
       Tone.Transport.start();
     }
@@ -62,6 +102,14 @@
       <input id="onsets" type="number" min=1 bind:value={onsets} />
       <label for="steps">Steps</label>
       <input id="steps" type="number" min=1 bind:value={steps} />
+      <label for="voice">Voice</label>
+      <select bind:value={voice}>
+        <option value="synth">Synth</option>
+        <option value="kick">Kick</option>
+        <option value="snare">Snare</option>
+        <option value="hihat">Hihat</option>
+        <option value="tom">Tom</option>
+      </select>
       <button on:click={handleGeneratePattern}>Generate Pattern</button>
       <button on:click={handlePlayPattern}>Play/Pause Pattern</button>
       <p>
