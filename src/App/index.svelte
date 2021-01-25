5a6eaddefa274d010171de5fe43394997020dfcb
Martin Maugeais <mrtnmgs@gmail.com>
2021-01-25 10:22:08 -0500

    Revert "mic"
    This reverts commit ad2485c705efdbaa3fbc423cb1c3b61aae283ad8.

--- src/App/index.svelte
+++ src/App/index.svelte
@@ -16,18 +16,6 @@
   function handleInit() {
     import('tone').then(module => {
       Tone = module
-    }).then(() => {
-
-      const mic = new Tone.UserMedia();
-      mic.open().then(() => {
-        // promise resolves when input is available
-        console.log("mic open");
-        // print the incoming mic levels in decibels
-      }).catch(e => {
-        // promise is rejected when the user doesn't have or allow mic access
-        console.log("mic not open");
-      });
-      mic.toDestination();
     });
   }
 
@@ -51,25 +39,25 @@
 
 <style src="./style.scss"></style>
 
-  <Layout>
-    <!--
-      <Circle pattern={p[0]} {active} ind=0 />
-      <Circle pattern={p[1]} {active} ind=1 />
-      <Circle pattern={p[2]} {active} ind=2 />
-      <Circle pattern={p[3]} {active} ind=3 />
-      <Circle pattern={p[4]} {active} ind=4 />
-    -->
-    {#if Tone}
-      <div class="left">
-        <div class="container__add-remove">
-          <button class="control-button" on:click={handleAddPattern}>Add</button>
-          <button class="control-button" on:click={handleRemovePattern}>Remove</button>
-        </div>
-        {#each Array(currentPatterns) as _, i}
-          <Euclidean {Tone} {tick} ind={i} />
-        {/each}
+<Layout>
+  <!--
+  <Circle pattern={p[0]} {active} ind=0 />
+  <Circle pattern={p[1]} {active} ind=1 />
+  <Circle pattern={p[2]} {active} ind=2 />
+  <Circle pattern={p[3]} {active} ind=3 />
+  <Circle pattern={p[4]} {active} ind=4 />
+  -->
+  {#if Tone}
+    <div class="left">
+      <div class="container__add-remove">
+        <button class="control-button" on:click={handleAddPattern}>Add</button>
+        <button class="control-button" on:click={handleRemovePattern}>Remove</button>
       </div>
-    {:else}
-      <button class="init-button" on:click={handleInit}>Start</button>
-    {/if}  
-  </Layout>
+      {#each Array(currentPatterns) as _, i}
+        <Euclidean {Tone} {tick} ind={i} />
+      {/each}
+    </div>
+  {:else}
+    <button class="init-button" on:click={handleInit}>Start</button>
+  {/if}  
+</Layout>
